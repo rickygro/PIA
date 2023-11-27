a6f8ee0886a97261603ed43824657cca6819984a
rickygro <142451944+rickygro@users.noreply.github.com>
2023-11-26 18:55:38 -0600

    Add files via upload

--- PIA_script.js
+++ PIA_script.js
@@ -2,14 +2,22 @@ document.addEventListener('DOMContentLoaded', function() {
     const colorChangeButton = document.querySelector('#color-change-button');
     const body = document.body;
 
+    const transitioning = (withTransition = true) => {
+        if (withTransition) {
+            body.classList.add('transitioning');
+            setTimeout(() => {
+                body.classList.remove('transitioning');
+            }, 1000);
+        }
+    };
+
     const savedColorMode = localStorage.getItem('colorMode');
     if (savedColorMode === 'white-mode') {
         body.classList.add('white-mode');
     }
 
     colorChangeButton.addEventListener('click', () => {
-        body.classList.add('transitioning');
-        
+        transitioning(); 
         if (body.classList.contains('white-mode')) {
             body.classList.remove('white-mode');
             localStorage.setItem('colorMode', '');
@@ -17,9 +25,9 @@ document.addEventListener('DOMContentLoaded', function() {
             body.classList.add('white-mode');
             localStorage.setItem('colorMode', 'white-mode');
         }
+    });
 
-        setTimeout(() => {
-            body.classList.remove('transitioning');
-        }, 1000); 
+    window.addEventListener('beforeunload', () => {
+        transitioning(false); 
     });
 });
